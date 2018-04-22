2d8eee1beb74914b30eaab53bee99f5432babfaa
unknown <Zihao.MAI@ALCN-7431719-L.as.corp.airliquide.com>
2018-04-22 17:43:31 +0800

    add more stuff

--- resources/HAProp.py
+++ resources/HAProp.py
@@ -5,7 +5,7 @@ Created on Fri Apr 20 22:42:29 2018
 @author: Zihao.MAI
 """
 
-from CoolProp.CoolProp import HAProps
+from CoolProp.CoolProp import HAPropsSI
 from flask_restful import Resource
 
 class HAProp(Resource):
--- resources/PropSI.py
+++ resources/PropSI.py
@@ -5,6 +5,21 @@ Created on Fri Apr 20 22:18:40 2018
 @author: Zihao.MAI
 """
 
+PATH_TO_REFPROP = 'd:\\REFPROP'
+REFPROP_READY = False
+
+from ..common.postimport import when_imported
+@when_imported('CoolProp.CoolProp')
+def setup_RPPath(mod):
+    mod.set_config_string(5, PATH_TO_REFPROP)
+    try:
+        mod.PropsSI('H', 'P', 101325, 'Q', 1, 'REFPROP::Water')
+    except:
+        REFPROP_READY = False
+    finally:
+        REFPROP_READY = True
+
+
 import CoolProp.CoolProp as cp
 from flask_restful import Resource
 
